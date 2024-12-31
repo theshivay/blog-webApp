aad99d3582165a9a52109ed8d570d7e70c52148b
Shivam Mishra <shivaymishraeee@gmail.com>
2024-12-31 07:35:30 +0530

    Reademe Done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -61,7 +61,7 @@ function Home() {
             </main>
             <footer className='bg-dark text-lg-start bottom-0'>
                 <div className='text-center p-3 text-white' style={{ backgroundColor: "rgba(0,0,0,0.2" }} >
-                    @ 2024 Copyright :
+                &copy; 2024 Copyright :
                     <a className='text-white mx-2' href="https://github.com/theshivay" alt="github.com/theshivay">Shivam Mishra</a>
                 </div>
             </footer>
